7153a98d746948faaba828cc211cbbe0600e697c
Rafael Costa <rafaelsevla@gmail.com>
2021-05-02 18:34:13 -0300

    add heroku config

--- backend/app.py
+++ backend/app.py
@@ -79,3 +79,8 @@ def past_launches():
     return {
         "launches": launches_to_response
     }, 200
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
