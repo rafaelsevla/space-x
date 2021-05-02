f28791c1991f3f504bea7c86a14bd6f93c34033c
Rafael Costa <rafaelsevla@gmail.com>
2021-05-02 17:11:49 -0300

    Add pagination to upcoming

--- frontend/src/upcoming-launches/upcoming-launches.tsx
+++ frontend/src/upcoming-launches/upcoming-launches.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Avatar, Card, CardContent, CardHeader, CardMedia, Grid, makeStyles, Typography } from '@material-ui/core';
+import { Avatar, Button, Card, CardContent, CardHeader, CardMedia, Grid, makeStyles, Typography } from '@material-ui/core';
 import { red } from '@material-ui/core/colors';
 
 import Base from '../common/ui/base';
@@ -17,52 +17,105 @@ const useStyles = makeStyles(() => ({
   avatar: {
     backgroundColor: red[500],
   },
+  paginationButton: {
+    marginTop: 30
+  }
 }));
 
 export default function UpcomingLaunches () {
   const classes = useStyles();
   const [ upcomingLaunches, setUpcomingLaunches ] = useState<AsyncData<Launch[]>>({ status: 'loading' });
+  const [ paginationOffset, setPaginationOffset ] = useState<number>(0);
+
+  async function fetchUpcomingLaunches (offset: number) {
+    try {
+      setUpcomingLaunches({ status: 'loading' })
+      const response: { data: { launches: Launch[] } } = await axios.get(
+        `http://localhost:5000/v1/launches/upcoming?offset=${paginationOffset}&limit=12`
+      );
+      console.log('paginationOffset', paginationOffset, response.data.launches)
+      setUpcomingLaunches({ status: 'loaded', data: response.data.launches });
+    } catch (err) {
+      setUpcomingLaunches({ status: 'error' });
+    }
+  }
 
   useEffect(() => {
-    (async () => {
-      try {
-        const response: { data: { launches: Launch[] } } = await axios.get('http://localhost:5000/v1/launches/upcoming?offset=0&limit=10');
-        setUpcomingLaunches({ status: 'loaded', data: response.data.launches });
-      } catch (err) {
-        setUpcomingLaunches({ status: 'error' });
-      }
-    })();
+    fetchUpcomingLaunches(paginationOffset);
   }, []);
+
+  function onPagination (action: 'next' | 'previous') {
+    let newPaginationOffset;
+    if (action === 'next') {
+      newPaginationOffset = paginationOffset + 10;
+    } else { // previous pagination
+      newPaginationOffset = paginationOffset - 10;
+    }
+    fetchUpcomingLaunches(newPaginationOffset);
+    setPaginationOffset(newPaginationOffset)
+  }
 
   return (
     <Base title='UPCOMING LAUNCHES' data={upcomingLaunches}>
       {upcomingLaunches.status === 'loaded' && (
-        <Grid container item xs={12} spacing={3}>
-          {upcomingLaunches.data.map(launch => (
-            <Grid item xs={4}>
-              <Card className={classes.root}>
-                <CardHeader
-                  avatar={
-                    <Avatar aria-label="recipe" className={classes.avatar}>
-                      {launch.flight_number}
-                    </Avatar>
-                  }
-                  title={`${launch.rocket.name} from ${launch.rocket.country}`}
-                  subheader={getFormatedDateWithYear(launch.date_utc)}
-                />
-                <CardMedia
-                  className={classes.media}
-                  image={launch.rocket.flickr_images[0]}
-                />
-                <CardContent>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                   {launch.details || 'Description coming soon...'}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+        <>
+          <Grid container item xs={12} spacing={3}>
+            {upcomingLaunches.data.length === 0 && (
+              <Typography variant='h4'>
+                There is no more release available
+              </Typography>
+            )}
+            {upcomingLaunches.data.map(launch => (
+              <Grid item xs={4}>
+                <Card className={classes.root}>
+                  <CardHeader
+                    avatar={
+                      <Avatar aria-label="recipe" className={classes.avatar}>
+                        {launch.flight_number}
+                      </Avatar>
+                    }
+                    title={`${launch.rocket.name} from ${launch.rocket.country}`}
+                    subheader={getFormatedDateWithYear(launch.date_utc)}
+                  />
+                  <CardMedia
+                    className={classes.media}
+                    image={launch.rocket.flickr_images[0]}
+                  />
+                  <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                    {launch.details || 'Description coming soon...'}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+
+          <Grid container item xs={12} spacing={3} className={classes.paginationButton}>
+          <Grid container xs={6}>
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={paginationOffset === 0}
+              onClick={() => onPagination('previous')}
+            >
+              PREVIOUS PAGE
+            </Button>
+          </Grid>
+
+          <Grid container xs={6} justify='flex-end'>
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={upcomingLaunches.data.length === 0}
+              onClick={() => onPagination('next')}
+            >
+              NEXT PAGE
+            </Button>
+          </Grid>
+
+          </Grid>
+        </>
       )}
     </Base>
   );
